ab43cf8d2aea72bf6892aa06a2101c28596e57ea
stf <7o5rfu92t@ctrlc.hu>
2025-10-15 21:47:32 +0200

    [enh] provision script now uses the shell interface

--- provision-ble.py
+++ provision-ble.py
@@ -3,7 +3,7 @@
 # SPDX-FileCopyrightText: 2025, Marsiske Stefan
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-import serial, binascii, sys, tomlkit, os, struct, pyudev
+import serial, binascii, sys, tomlkit, os, struct, pyudev, time, shutil
 from tempfile import NamedTemporaryFile
 from esptool.cmds import detect_chip
 from pysodium import crypto_generichash, randombytes
@@ -27,6 +27,7 @@ def write_cfg(cfg, cfg_file, mac, spk, npk, noise_sk, uart):
    for k, server in cfg.get('servers',{}).items():
       if k == name:
          print(f'warning: there is already a server configured with the name "{name}", will overwrite values in there', file=sys.stderr)
+         shutil.copy2(cfg_file, f"{cfg_file}.bak")
          table = server
       elif uart is None and server.get('bleaddr') == mac:
          print(f'warning: this config already has a server configured with the name: "{k}"\n'
@@ -65,7 +66,7 @@ def main():
          cfg_file = arg
       elif arg.endswith('authorized_keys'):
          with open(arg,'r') as fd:
-            authkeys=[binascii.a2b_base64(line.strip()) for line in fd]
+            authkeys=[line.strip() for line in fd]
       elif arg=='uart':
          uart=get_serial(PORT)
       elif arg=="esp":
@@ -80,39 +81,60 @@ def main():
          esp.connect()
          esp.hard_reset()
 
-   serialPort = serial.Serial(port=PORT, baudrate=115200)
+   serialPort = serial.Serial(port=PORT, baudrate=115200, timeout=0.5)
    for _ in range(100):
       line = serialPort.readline().decode("Ascii").strip()
       if debug: print(line)
-      if 'no configuration found. waiting for initialization' in line:
-         ltsigpub = binascii.a2b_base64(cfg['client']['ltsigpub'][8:])
+      if 'no configuration found. waiting for client initialization' in line:
+         print("device is unintialized. sending client config", file=sys.stderr)
+         serialPort.reset_input_buffer()
+         ltsigpub = cfg['client']['ltsigpub'][8:]
+         serialPort.write(f'init ltsig {ltsigpub}\n'.encode('utf8'))
+         time.sleep(0.1)
          noise_sk = randombytes(32)
          noise_pk = noisexk.pubkey(noise_sk)
-         msg = ltsigpub+noise_pk+struct.pack("B",len(authkeys))+b''.join(authkeys)
-         serialPort.write(b'KLUTSHNIK-DEVICE-INIT'+struct.pack("!H", len(msg))+msg)
+         serialPort.write(f'init noise {binascii.b2a_base64(noise_pk).decode('utf8').strip()}\n'.encode('utf8'))
+         time.sleep(0.1)
+         for ak in authkeys:
+             if ak.strip() == '': continue
+             serialPort.write(f"authkey add {ak}\n".encode('utf8'))
+             time.sleep(0.1)
          break
    else:
        raise ValueError("unexpected initialization")
 
+   print("waiting a bit for device to generate its own keys", file=sys.stderr)
+   time.sleep(0.4)
+   # todo maybe check if any of the init ops gave any negative results
+   serialPort.reset_input_buffer()
    # collect info
-   npk=None
-   spk=None
-   mac=None
-   for _ in range(100):
-      line = serialPort.readline().decode("Ascii").strip()
-      if debug: print(line)
-      if '<inf> klutshnik: noise pk' in line:
-         npk=binascii.a2b_base64(line.split()[-1])
-      if '<inf> klutshnik: ltsig pk' in line:
-         spk=binascii.a2b_base64(line.split()[-1])
-      if '<inf> klutshnik: MAC address:' in line:
-         mac=line.split()[-2]
-      if spk is not None and npk is not None and (uart is not None or mac is not None):
-         return write_cfg(cfg, cfg_file, mac,spk,npk, noise_sk, uart)
-   if uart is None:
-      print('failed to fetch the pubkeys and mac')
+   serialPort.write(b'getcfg noisepk\n')
+   time.sleep(0.1)
+   _ = serialPort.readline().decode("Ascii").strip()
+   line = serialPort.readline().decode("Ascii").strip()
+   npk=binascii.a2b_base64(line.split()[-1])
+
+   serialPort.reset_input_buffer()
+   serialPort.write(b'getcfg ltsigpk\n')
+   time.sleep(0.1)
+   _ = serialPort.readline().decode("Ascii").strip()
+   line = serialPort.readline().decode("Ascii").strip()
+   spk=binascii.a2b_base64(line.split()[-1])
+
+   serialPort.reset_input_buffer()
+   serialPort.write(b'getcfg mac\n')
+   time.sleep(0.1)
+   _ = serialPort.readline().decode("Ascii").strip()
+   line = serialPort.readline().decode("Ascii").strip()
+   if line == "No MAC, this klutshnik device doesn't do BLE":
+       mac=None
+   elif line.startswith('MAC address: '):
+       mac=line.split()[-2]
    else:
-      print('failed to fetch the pubkeys')
+       print(line,file=sys.stderr)
+       raise ValueError("failed to retrieve mac address from device")
+
+   return write_cfg(cfg, cfg_file, mac,spk,npk, noise_sk, uart)
 
 if __name__ == '__main__':
     main()
