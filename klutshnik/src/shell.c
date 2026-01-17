a611fc707ca8615eb159f4330b9fc358cf50a510
stf <7o5rfu92t@ctrlc.hu>
2026-01-17 02:46:48 +0100

    [enh] added an initcfg shell cmd which initializes the device keys, and can check if the device is initialized

--- klutshnik/src/shell.c
+++ klutshnik/src/shell.c
@@ -6,6 +6,7 @@
 #include <zephyr/fs/fs.h>
 #include <zephyr/shell/shell.h>
 #include <zephyr/sys/base64.h>
+#include <zephyr/drivers/entropy.h>
 #ifdef CONFIG_KLUTSHNIK_BLE
 #include <zephyr/bluetooth/bluetooth.h>
 #endif
@@ -13,6 +14,8 @@
 #include <sodium.h>
 #include "klutshnik.h"
 #include "XK.h"
+
+// todo log(in|out) commands based on challenge-response signed by owner_key
 
 static int getcfg_cmd_handler(const struct shell *sh, size_t argc, char **argv, void *data) {
   int cfgtype = (int)data;
@@ -123,19 +126,21 @@ static int cmd_init_noise(const struct shell *sh, size_t argc, char **argv) {
     shell_error(sh, "Error: must provide exactly one 44 byte long base64 encoded client noise pubkey as parameter");
     return -1;
   }
-  uint8_t noisepk[32];
+  uint8_t rec_buf[sizeof(NoiseRec)];
+  NoiseRec *noise_rec=(NoiseRec *) rec_buf;
+  noise_rec->refs=0xffffffff;
   size_t olen;
-  int err = base64_decode (noisepk, sizeof noisepk, &olen, argv[1], slen);
+  int err = base64_decode (noise_rec->key, sizeof noise_rec->key, &olen, argv[1], slen);
   if(err!=0) {
     shell_error(sh, "Error: failed to decode base64 encoded noise pubkey: %d", err);
     return -1;
   }
-  if(olen!=sizeof noisepk) {
-    shell_error(sh, "Error: decoded noisepk is too short, only %d, instead of expected %d", olen, sizeof noisepk);
+  if(olen!=sizeof noise_rec->key) {
+    shell_error(sh, "Error: decoded noisepk is too short, only %d, instead of expected %d", olen, sizeof noise_rec->key);
     return -1;
   }
   fs_mkdir("/lfs/cfg");
-  err=save("/lfs/cfg/authorized_clients", sizeof noisepk, noisepk, 0);
+  err=save("/lfs/cfg/authorized_clients", sizeof rec_buf, rec_buf, FS_O_APPEND);
   if(0!=err) {
     shell_error(sh, "failed to save authorized client noise key of initialing client: %d. aborting.", err);
     return -1;
@@ -151,7 +156,7 @@ static int cmd_init_check(const struct shell *sh, size_t argc, char **argv) {
     return 0;
   }
 
-  for(int i=0;i<3;i++) {
+  for(int i=0;i<6;i++) {
     int rc = (res >> (i*2)) & 3;
     switch(rc) {
     case 1: {
@@ -172,13 +177,86 @@ static int cmd_init_check(const struct shell *sh, size_t argc, char **argv) {
   return -1;
 }
 
+static int initkey(const struct shell *sh, const char *path, const size_t key_len, uint8_t *key) {
+  struct fs_dirent entry;
+  if(0 == fs_stat(path, &entry)) {
+    shell_warn(sh, "W %s does exist", path);
+    if(entry.size!=key_len) {
+      shell_error(sh, "%s has invalid size", path);
+      fs_unlink(path);
+    } else {
+      return 0; //-EEXIST;
+    }
+  }
+
+  const struct device *rng_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_entropy));
+  if (!device_is_ready(rng_dev)) {
+    shell_error(sh, "error: random device not ready");
+    int i;
+    for(i=0;!device_is_ready(rng_dev) && i<10;i++) k_sleep(K_MSEC(10));
+    if(i>=10) return -EAGAIN;
+  }
+  entropy_get_entropy(rng_dev, (char *)key, key_len);
+  crypto_generichash(key,key_len, key,key_len, NULL,0);
+
+  return save(path,key_len,key,0);
+}
+
+static int initcfg(const struct shell *sh, size_t argc, char **argv) {
+  fs_mkdir("/lfs/cfg");
+
+  int rc;
+  uint8_t noise_sk[32];
+  rc = initkey(sh, "/lfs/cfg/noise_key", 32, noise_sk);
+  if(rc!=0) {
+    shell_error(sh, "E failed to init noise_sk");
+    return rc;
+  }
+
+  uint8_t ltsig_seed[crypto_sign_SEEDBYTES];
+  rc = initkey(sh, "/lfs/cfg/ltsig_seed", crypto_sign_SEEDBYTES, ltsig_seed);
+  if(rc!=0) {
+    shell_error(sh, "E failed to init ltsig_seed");
+    return rc;
+  }
+  uint8_t dummy[crypto_sign_PUBLICKEYBYTES];
+  uint8_t ltsig_sk[crypto_sign_SECRETKEYBYTES];
+  if(0!=crypto_sign_seed_keypair(dummy, ltsig_sk, ltsig_seed)) {
+    shell_error(sh, "E failed to derive ltsig keypair");
+    return -1;
+  }
+  sodium_memzero(ltsig_seed,sizeof ltsig_seed);
+
+  uint8_t authkey_pk[64];
+  crypto_sign_ed25519_sk_to_pk(authkey_pk, ltsig_sk);
+  Noise_XK_dh_secret_to_public(authkey_pk+32, noise_sk);
+  rc = save("/lfs/cfg/authorized_keys", 64, authkey_pk, FS_O_APPEND);
+  if(0!=rc) {
+    shell_error(sh, "failed to save authorized keys of device: %d. aborting.", rc);
+    return rc;
+  }
+
+  uint8_t rec_salt[32];
+  rc = initkey(sh, "/lfs/cfg/record_salt", 32, rec_salt);
+  if(rc!=0) {
+    shell_error(sh, "E failed to init record salt");
+    return rc;
+  }
+
+  shell_print(sh, "initialized device secrets");
+  return 0;
+}
+
 SHELL_STATIC_SUBCMD_SET_CREATE(sub_init,
-                               SHELL_CMD_ARG(ltsig, NULL, "Set owner ltsig pubkey.", cmd_init_ltsig, 2, 0),
-                               SHELL_CMD_ARG(noise, NULL, "Set owner noise pubkey.", cmd_init_noise, 2, 0),
-                               SHELL_CMD(check, NULL, "Check if config is complete.", cmd_init_check),
+                               SHELL_CMD_ARG(ltsig, NULL, SHELL_HELP("Set owner ltsig pubkey.",
+                                                                     "<base64 encoded ltsig pubkey>"), cmd_init_ltsig, 2, 0),
+                               SHELL_CMD_ARG(noise, NULL, SHELL_HELP("Set owner noise pubkey.",
+                                                                     "<base64 encoded noise pubkey>"), cmd_init_noise, 2, 0),
+                               SHELL_CMD(check, NULL, SHELL_HELP("Check if config is complete.", ""), cmd_init_check),
                                SHELL_SUBCMD_SET_END
 );
-SHELL_CMD_REGISTER(init, &sub_init, "initialize klutshnik device", NULL);
+SHELL_CMD_REGISTER(init, &sub_init, SHELL_HELP("initialize klutshnik device", " <> | [ltsig <b64>] | [noise <b64>] | [check]\n"
+                                               "without param initializes the device keys"), initcfg);
 
 
 static int cmd_authkey_add(const struct shell *sh, size_t argc, char **argv) {
